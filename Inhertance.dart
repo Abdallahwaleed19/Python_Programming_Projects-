39d4e13ebad51ae1ade958007986ba67538d98fb
Abdallah Waleed Kamal <waleedabdallah238@gmail.com>
2025-03-13 17:18:30 +0200

    Dart

---
+++ Inhertance.dart
@@ -0,0 +1,34 @@
+// class Person {
+//   String? name;
+//   int? age;
+
+//   void display() {
+//     print("The person name is : $name , and his age is : $age ");
+//   }
+// }
+
+// class Employee extends Person {
+//   int? employeeId;
+//   String? companyName;
+//   String? departmentName;
+
+//   void displayInfo() {
+//     print("The Employee Name : $name , and his Age : $age");
+//     print(
+//       "The Employee Id : $employeeId , and his Company Name : $companyName , and his Department is : $departmentName ",
+//     );
+//   }
+// }
+
+// void main() {
+//   var employee = Employee();
+//   employee.name = "Abdallah";
+//   employee.age = 20;
+//   employee.employeeId = 20230192;
+//   employee.companyName = "Menoufia National University";
+//   employee.departmentName = "IoT Teaching Assistant";
+//   employee.display();
+//   employee.displayInfo();
+// }
+
+
